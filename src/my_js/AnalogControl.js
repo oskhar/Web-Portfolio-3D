19d0a329e2c78192e2670e3948f62fe72ec936a2
Muhamad Oskhar <118426376+MuhamadOskhar@users.noreply.github.com>
2023-02-02 19:21:50 +0700

    Penambahan kontrol analog untuk mobile device

---
+++ src/my_js/AnalogControl.js
@@ -0,0 +1,39 @@
+// Class
+class AnalogControl {
+
+    // Constructor
+    constructor () {
+
+        // Atribute
+        this.pin = [];
+        this.keyboard = [];
+        this.touch = false;
+        this.xAnalog = 0;
+        this.yAnalog = 0;
+
+    }
+
+    // Method
+    addToDom () {
+
+        for (let i = 0; i < 9; i++) {
+
+            this.pin[i] = document.createElement('div');
+            this.pin[i].id = "pin"+i;
+            this.pin[i].style.position = "absolute";
+            this.pin[i].style.height = (Math.floor(innerWidth / 6) - 80) +"px";
+            this.pin[i].style.width = (Math.floor(innerWidth / 6) - 80) +"px";
+            this.pin[i].style.background = "transparent";
+            this.pin[i].style.borderRadius = "50%";
+            this.pin[i].style.border = "20px solid rgba(255, 255, 255, 0.3)";
+            this.pin[i].style.top = (innerHeight - innerWidth/2) + (Math.floor((i) / 3) * Math.floor(innerWidth/6)) + "px";
+            this.pin[i].style.left = ((((i % 3) * Math.floor(innerWidth / 6) + (Math.ceil(i%3) * 20))+20) + (innerWidth / 2 - 50)) + "px";
+            document.body.appendChild(this.pin[i]);
+
+        }
+        
+    }
+
+}
+
+export {AnalogControl};
